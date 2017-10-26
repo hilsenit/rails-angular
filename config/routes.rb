02657977f82129a080877680a14fffe476b83d96
peterhoejlundandersen <hilsenit@gmail.com>
2017-10-26 19:42:38 +0200

    trying to run javascript test and designed som basic layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :products, only: :index
-  root 'dashboard#index'
-
+  get 'dashboard' => 'dashboard#index'
+  get 'om-mikrofest' => 'pages#about', as: :about
+  get 'kontakt' => 'pages#contact', as: :contact
+  root 'pages#index'
 end
