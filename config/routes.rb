7c808d1c241f4dcde3ffe3dd02fd1b2dd94d4e21
peterhoejlundandersen <hilsenit@gmail.com>
2017-10-30 13:56:36 +0100

    added lorem text to footer - installed plugin loremipsum

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
   resources :products, :orders, :publishers, :blog_posts
   get 'admin' => 'admin#dashboard'
-=======
-  resources :products, only: [:index, :new]
-  get 'dashboard' => 'dashboard#index', as: :dashboard
->>>>>>> 6aff0183436088cd4a66efb6b64d4c7dd56661b8
   get 'om-mikrofest' => 'pages#about', as: :about
   get 'kontakt' => 'pages#contact', as: :contact
   root 'pages#index'
